3bde21104419ea9f531caff28c3671b77a2fb991
joelDP20 <56432418+joelDP20@users.noreply.github.com>
2019-10-11 04:06:05 -0500

    Add files via upload

---
+++ Conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$host ='localhost';
+$user ='root';
+$password ='';
+$db ='login';
+
+$conection =@mysqli_connect($host,$user,$password,$db);
+
+if(!$conection){
+    echo "Error";
+}else{
+    echo "";
+}
